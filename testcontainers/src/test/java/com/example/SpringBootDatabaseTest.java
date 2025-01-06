80a5f65e092f8bea3bfe833a058bc561f6ebf742
Anders Swanson <anders.swanson@oracle.com>
2025-01-06 13:41:04 -0800

    Testcontainers ServiceConnection
    Signed-off-by: Anders Swanson <anders.swanson@oracle.com>

--- testcontainers/src/test/java/com/example/SpringBootDatabaseTest.java
+++ testcontainers/src/test/java/com/example/SpringBootDatabaseTest.java
@@ -6,17 +6,13 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.Duration;
 
-import oracle.jdbc.pool.OracleDataSource;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.oracle.OracleContainer;
-import org.testcontainers.utility.MountableFile;
 
 @Testcontainers
 @SpringBootTest
@@ -25,18 +21,11 @@ public class SpringBootDatabaseTest {
      * Use a containerized Oracle Database instance for testing.
      */
     @Container
+    @ServiceConnection
     static OracleContainer oracleContainer = new OracleContainer("gvenzl/oracle-free:23.5-slim-faststart")
             .withStartupTimeout(Duration.ofMinutes(5))
             .withUsername("testuser")
             .withPassword("testpwd");
-
-    @DynamicPropertySource
-    static void registerProperties(DynamicPropertyRegistry registry) {
-        // Dynamically configure Spring Boot properties to use the Testcontainers database.
-        registry.add("spring.datasource.url", oracleContainer::getJdbcUrl);
-        registry.add("spring.datasource.username", oracleContainer::getUsername);
-        registry.add("spring.datasource.password", oracleContainer::getPassword);
-    }
 
     @Autowired
     DataSource dataSource;

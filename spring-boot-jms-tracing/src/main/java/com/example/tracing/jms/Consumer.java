286ec1874b3200b2c6150761fd530b7c8c754310
Anders Swanson <anders.swanson@oracle.com>
2025-10-06 13:25:47 -0700

    Remove latch
    Signed-off-by: Anders Swanson <anders.swanson@oracle.com>

--- spring-boot-jms-tracing/src/main/java/com/example/tracing/jms/Consumer.java
+++ spring-boot-jms-tracing/src/main/java/com/example/tracing/jms/Consumer.java
@@ -12,19 +12,8 @@ import org.springframework.stereotype.Component;
 public class Consumer {
     private static final Logger log = LoggerFactory.getLogger(Consumer.class);
 
-    private final CountDownLatch latch;
-
-    public Consumer(@Value("${txeventq.consumer.numMessages:5}") int numMessages) {
-        latch = new CountDownLatch(numMessages);
-    }
-
     @JmsListener(destination = "${txeventq.queue.name:testqueue}", id = "sampleConsumer")
     public void receiveMessage(String message) {
         log.info("Received Message: {}", message);
-        latch.countDown();
-    }
-
-    public void await() throws InterruptedException {
-        latch.await();
     }
 }

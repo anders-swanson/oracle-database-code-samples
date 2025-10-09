5d4a357c6276a11844392eb2bdb77d729e68cfff
Anders Swanson <anders.swanson@oracle.com>
2025-10-09 12:09:58 -0700

    update
    Signed-off-by: Anders Swanson <anders.swanson@oracle.com>

--- spring-boot-jdbc-tracing/src/main/java/com/example/tracing/jdbc/JDBCTracingApp.java
+++ spring-boot-jdbc-tracing/src/main/java/com/example/tracing/jdbc/JDBCTracingApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JDBCTracingApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(JDBCTracingApp.class, args);
     }
 }
